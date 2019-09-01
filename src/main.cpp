dc5fc3fa3e65faac338ef5b25ad0993aa49f7158
PathosBill <trayduncan@gmail.com>
2019-08-31 23:10:27 -0500

    refactor main in prep for calibration

--- src/main.cpp
+++ src/main.cpp
@@ -3,91 +3,24 @@
 #include <RGBmatrixPanel.h> // Hardware-specific library
 #include <SPI.h>
 
+//define output pins for RGBmatrix
 #define CLK 11 // MUST be on PORTB! (Use pin 11 on Mega)
 #define LAT A11
 #define OE 9
 #define A A8
 #define B A9
 #define C A10
+// define pot input
 #define POTA A0
 #define POTB A1
 #define POTC A2
 #define POTD A3
 #define POTE A4
 #define POTF A5
-#define NUM_READS 1000
 
-int val = 0;
-
+// global variables
 int mappedVal = 0;
-int curVal = 0;
-int avgVal = 0;
 int hue = 0;
-float temperatureRead = 0.0;
-
-int smoothShower = 0;
-int smoothToilet = 0;
-int smoothWater = 0;
-int smoothDishes = 0;
-int smoothLaundry = 0;
-int smoothLawn = 0;
-
-float reduceNoise(int mappedValue)
-{
-  // read multiple values and sort them to take the mode
-  int sortedValues[NUM_READS];
-  for (int i = 0; i < NUM_READS; i++)
-  {
-    int value = mappedValue;
-    int j;
-    if (value < sortedValues[0] || i == 0)
-    {
-      j = 0; //insert at first position
-    }
-    else
-    {
-      for (j = 1; j < i; j++)
-      {
-        if (sortedValues[j - 1] <= value && sortedValues[j] >= value)
-        {
-          // j is insert position
-          break;
-        }
-      }
-    }
-    for (int k = i; k > j; k--)
-    {
-      // move all values higher than current reading up one position
-      sortedValues[k] = sortedValues[k - 1];
-    }
-    sortedValues[j] = value; //insert current reading
-  }
-  //return scaled mode of 10 values
-  float returnval = 0;
-  for (int i = NUM_READS / 2 - 5; i < (NUM_READS / 2 + 5); i++)
-  {
-    returnval += sortedValues[i];
-  }
-  returnval = returnval / 10;
-  return returnval * 1100 / 1023;
-}
-
-int smooth(int data, float filterVal, float smoothedVal)
-{
-
-  if (filterVal > 1)
-  { // check to make sure param's are within range
-    filterVal = .99;
-  }
-  else if (filterVal <= 0)
-  {
-    filterVal = 0;
-  }
-
-  smoothedVal = (data * (1 - filterVal)) + (smoothedVal * filterVal);
-
-  return (int)smoothedVal;
-}
 
 RGBmatrixPanel matrix(A, B, C, CLK, LAT, OE, true);
 
@@ -108,60 +41,7 @@ void loop()
   int laundry = (analogRead(POTE));
   int lawn = (analogRead(POTD));
 
-  smoothShower = smooth(shower, .9, smoothShower);
-  smoothToilet = smooth(toilet, .9, smoothToilet);
-  smoothWater = smooth(water, .9, smoothWater);
-  smoothDishes = smooth(dishes, .9, smoothDishes);
-  smoothLaundry = smooth(laundry, .9, smoothLaundry);
-  smoothLawn = smooth(lawn, .9, smoothLawn);
-
-  int mappedShower = (map(shower, 50, 950, 40, 0));
-
-  if (mappedShower >= 43)
-  {
-    mappedShower = 43;
-  }
-  else if (mappedShower < 4)
-  {
-    mappedShower = 0;
-  }
-
-  int mappedToilet = (map(smoothToilet, 118, 940, 50, 0));
-
-  int mappedWater = (map(smoothWater, 540, 930, 40, 0));
-
-  int mappedDishes = (map(smoothDishes, 69, 949, 8, 0));
-
-  if (mappedDishes < 2)
-  {
-    mappedDishes = 0;
-  }
-  else if (mappedDishes > 8)
-  {
-    mappedDishes = 8;
-  }
-
-  int mappedLaundry = (map(smoothLaundry, 80, 800, 900, 0));
-
-  if (mappedLaundry < 10)
-  {
-    mappedLaundry = 0;
-  }
-  else if (mappedLaundry > 900)
-  {
-    mappedLaundry = 900;
-  }
-  int mappedLawn = (map(smoothLawn, 51, 786, 28, 0));
-  if (mappedLawn < 2)
-  {
-    mappedLawn = 0;
-  }
-  else if (mappedLawn > 28)
-  {
-    mappedLawn = 28;
-  }
-
-  mappedVal = (mappedShower + (mappedToilet * 0.16) + mappedWater + mappedDishes + mappedLaundry + mappedLawn);
+  mappedVal = (shower);
   //mappedVal= mappedWater;
   // mappedVal= water;
 
